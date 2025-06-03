36848e3d5a3c62425d213ad8e2261fb7b5e0e9d9
devcambo <devcambo001@gmail.com>
2025-06-03 13:47:57 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# expr-SpringCloudAwsS3
---
+++ src/main/java/com/devcambo/cloudawss3/Cloudawss3Application.java
@@ -0,0 +1,13 @@
+package com.devcambo.cloudawss3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cloudawss3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cloudawss3Application.class, args);
+    }
+
+}
